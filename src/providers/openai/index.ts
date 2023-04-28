9083716b9c44b715140f5695a153e0059d956819
vaan89 <125148569+vaan89@users.noreply.github.com>
2023-04-28 14:48:56 +0800

    Update index.ts

--- src/providers/openai/index.ts
+++ src/providers/openai/index.ts
@@ -14,20 +14,20 @@ const providerOpenAI = () => {
     globalSettings: [
       {
         key: 'apiKey',
-        name: 'API Key',
+        name: '填写Key',
         type: 'api-key',
       },
       {
         key: 'baseUrl',
-        name: 'Base URL',
-        description: 'Custom base url for OpenAI API.',
+        name: '接口地址',
+        description: 'openai接口地址（勿动）',
         type: 'input',
         default: 'https://api.openai.com',
       },
       {
         key: 'model',
-        name: 'OpenAI model',
-        description: 'Custom gpt model for OpenAI API.',
+        name: '选择模型',
+        description: '选择聊天模型',
         type: 'select',
         options: [
           { value: 'gpt-3.5-turbo', label: 'gpt-3.5-turbo' },
@@ -37,8 +37,8 @@ const providerOpenAI = () => {
       },
       {
         key: 'maxTokens',
-        name: 'Max Tokens',
-        description: 'The maximum number of tokens to generate in the completion.',
+        name: '最大对话字节',
+        description: '对话的最大字节限制.',
         type: 'slider',
         min: 0,
         max: 4096,
@@ -47,9 +47,9 @@ const providerOpenAI = () => {
       },
       {
         key: 'temperature',
-        name: 'Temperature',
+        name: '随机程度',
         type: 'slider',
-        description: 'What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.',
+        description: '0-2 越大回复越随机',
         min: 0,
         max: 2,
         default: 0.7,
@@ -57,8 +57,8 @@ const providerOpenAI = () => {
       },
       {
         key: 'top_p',
-        name: 'Top P',
-        description: 'An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.',
+        name: '回复质量',
+        description: '回复质量0-1(勿动).',
         type: 'slider',
         min: 0,
         max: 1,
