5465fa63e4958fc30f9a084d55ee7e88b6ade4e2
vaan89 <125148569+vaan89@users.noreply.github.com>
2023-05-11 11:35:59 +0800

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,12 +7,12 @@ export default () => {
     <header onDblClick={scrollController().scrollToTop} class="shrink-0 absolute top-0 left-0 right-0 fi justify-between bg-base-100 border-b border-base h-14 px-4">
       <div class="fi overflow-hidden">
         
-           <ul class="mobile-nav mobile-nav-bottom">
-  <li><a href="https://www.u8ai.com">AI工具</a></li>
-  <li><a href="https://ai.u8ai.com/">AI绘画</a>
-</li>
-  <li><a href="https://www.u8ai.com/?cat=20">内容</a></li>
+         <ul class="mobile-nav mobile-nav-bottom" style="text-align:center;margin-top:20px;padding-left:0;">
+  <li style="display:inline-block;margin:0 10px;vertical-align:middle;"><a href="https://www.u8ai.com">AI工具</a></li>
+  <li style="display:inline-block;margin:0 10px;vertical-align:middle;"><a href="https://ai.u8ai.com/">AI绘画</a></li>
+  <li style="display:inline-block;margin:0 10px;vertical-align:middle;"><a href="https://www.u8ai.com/?cat=20">内容</a></li>
 </ul>
+
         <div
           class="fcc p-2 rounded-md text-xl hv-foreground md:hidden"
           onClick={() => showConversationSidebar.set(true)}
