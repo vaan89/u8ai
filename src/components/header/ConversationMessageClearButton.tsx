21680d4325cb3b4a8578797ed1b26fef90fbfa23
vaan89 <125148569+vaan89@users.noreply.github.com>
2023-05-11 12:01:47 +0800

    Update ConversationMessageClearButton.tsx

--- src/components/header/ConversationMessageClearButton.tsx
+++ src/components/header/ConversationMessageClearButton.tsx
@@ -17,11 +17,7 @@ export default () => {
         <div
           class="fcc p-2 rounded-md text-xl hv-foreground"
           onClick={handleClearMessage}
-        >  <ul class="mobile-nav mobile-nav-bottom" style="text-align:center;margin-top:20px;padding-left:0;">
-  <li style="display:inline-block;margin:0 10px;vertical-align:middle;"><a href="https://www.u8ai.com">AI工具</a></li>
-  <li style="display:inline-block;margin:0 10px;vertical-align:middle;"><a href="https://ai.u8ai.com/">AI绘画</a></li>
-  <li style="display:inline-block;margin:0 10px;vertical-align:middle;"><a href="https://www.u8ai.com/?cat=20">内容</a></li>
-</ul>
+        > 
           <div i-carbon-clean />
         </div>
       )}
